e09ea13a8c4ce11a926d81ea9205357b0c45d110
Saim7787 <ujamil503@gmail.com>
2024-02-04 19:12:37 +0500

    add graph

--- frontend/src/admin/Dashboard.jsx
+++ frontend/src/admin/Dashboard.jsx
@@ -1,7 +1,55 @@
-import React from 'react'
+
 import CircularProgress from '../components/Progress/Progress'
+import React, { PureComponent } from 'react';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+
 
 const Dashboard = () => {
+  const data = [
+    {
+      name: 'Mon',
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: 'Tue',
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: 'Wed',
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: 'Thu',
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: 'Fri',
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: 'Sat',
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: 'Sun',
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
+  
   return (
     <div className="max-w-[85rem] px-1 py-10 sm:px-6 lg:px-2 lg:py-4 mx-auto">
 
@@ -70,69 +118,39 @@ const Dashboard = () => {
         </a>
 
 
-        {/* <a className="group flex flex-col bg-[#676767]  border-none shadow-sm rounded-md hover:shadow-md  dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">
-          <div className="p-4 md:p-5">
-            <div className="flex justify-between items-center">
-              <div>
-                <h3 className="text-xl xs:text-2xl sm:text-3xl font-regular tracking-wide">
-                  Customer
-                </h3>
-                <p className="text-xl xs:text-2xl sm:text-3xl font-bold tracking-wide">
-                  256,356
-                </p>
-
-                <div className="text-sm w-16  bg-black my-2 text-center xs:text-xl sm:text-xl font-semibold tracking-wide ">
-                  +1.9K
-                </div>
-              </div>
-              <div className="ps-3">
-                <CircularProgress
-                  identifier="development4"
-                  startValue={0}
-                  endValue={80}
-                  speed={30}
-                  circleColor="#B600D4"
-                />
-
-              </div>
-            </div>
-          </div>
-        </a>
-
-        <a className="group flex flex-col bg-[#676767]  border-none shadow-sm rounded-md hover:shadow-md  dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">
-          <div className="p-4 md:p-5">
-            <div className="flex justify-between items-center">
-              <div>
-                <h3 className="text-xl xs:text-2xl sm:text-3xl font-regular tracking-wide">
-                  Customer
-                </h3>
-                <p className="text-xl xs:text-2xl sm:text-3xl font-bold tracking-wide">
-                  256,356
-                </p>
-
-                <div className="text-sm w-16  bg-black my-2 text-center xs:text-xl sm:text-xl font-semibold tracking-wide ">
-                  +1.9K
-                </div>
-              </div>
-              <div className="ps-3">
-                <CircularProgress
-                  identifier="development5"
-                  startValue={0}
-                  endValue={80}
-                  speed={30}
-                  circleColor="#B600D4"
-                />
-
-              </div>
-            </div>
-          </div>
-        </a> */}
+     
 
 
 
       </div>
 
+      <div style={{ width: '100%', height: 300 }}>
+     <ResponsiveContainer>
+          <AreaChart
+            data={data}
+            margin={{
+              top: 10,
+              right: 0,
+              left: 0,
+              bottom: 0,
+            }}
+            
+          >
+            <CartesianGrid strokeDasharray="3 3"  />
+            <XAxis dataKey="name" />
+           
+            <Tooltip  />
+            <Area type="monotone" dataKey="uv" stroke="#B600D4" fill="#B600D4"  />
+          </AreaChart>
+        </ResponsiveContainer>
 
+
+      </div>
+
+
+ 
+
+   
 
       <div className="grid sm:grid-cols-1 my-5 sm:grid-cols-2 md:grid-cols-3  gap-3 sm:gap-6">
       <div className="bg-[#474747] px-4 py-4 rounded-md">
