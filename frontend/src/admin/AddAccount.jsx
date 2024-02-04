1807433426edc36b33b2f2029c4fa9362be9de5a
Suheer Zahid <150055579+CodeWithSuheer@users.noreply.github.com>
2024-02-04 14:11:22 +0500

    all clear

--- frontend/src/admin/AddAccount.jsx
+++ frontend/src/admin/AddAccount.jsx
@@ -14,12 +14,12 @@ const AddAccount = () => {
                         {/* ------------ INPUT FIELDS ------------ */}
                         <div>
                             <label className="block mb-2 text-md font-medium text-gray-200">Account Number</label>
-                            <input type="text" placeholder="Enter Your Name" className="block w-full px-5 py-4 mt-2 border rounded-lg placeholder-[#b4b4b4] bg-transparent text-gray-300 border-[#B600D4] focus:border-[#B600D4] focus:border-[#B600D4] focus:ring-[#B600D4] focus:outline-none focus:ring focus:ring-opacity-40" />
+                            <input type="text" placeholder="Enter Your Name" className="block w-full px-5 py-4 mt-2 border rounded-lg placeholder-[#b4b4b4] bg-transparent text-gray-300 border-[#B600D4]  focus:border-[#B600D4] focus:ring-[#B600D4] focus:outline-none focus:ring focus:ring-opacity-40" />
                         </div>
 
                         <div>
                             <label className="block mb-2 text-md font-medium text-gray-200">Account Title</label>
-                            <input type="text" placeholder="Enter Your Account Title" className="block w-full px-5 py-4 mt-2 border rounded-lg placeholder-[#b4b4b4] bg-transparent text-gray-300 border-[#B600D4] focus:border-[#B600D4] focus:border-[#B600D4] focus:ring-[#B600D4] focus:outline-none focus:ring focus:ring-opacity-40" />
+                            <input type="text" placeholder="Enter Your Account Title" className="block w-full px-5 py-4 mt-2 border rounded-lg placeholder-[#b4b4b4] bg-transparent text-gray-300 border-[#B600D4] focus:border-[#B600D4] focus:ring-[#B600D4] focus:outline-none focus:ring focus:ring-opacity-40" />
                         </div>
 
                     </div>
@@ -29,7 +29,7 @@ const AddAccount = () => {
                             <span className='h-8 w-8 p-[3px] bg-white rounded-md mr-2'>
                                 <img className='rounded-md' src="https://play-lh.googleusercontent.com/9-0wlkGycWAJRsuQ-p_bMqDGE0liYgihxKka0PtRjxqEiRVkDKaROEyFxYg520lLbpk" alt="" />
                             </span>
-                            <span className="text-lg mr-3 text-gray-500 text-[#cfcfcf]">Jazz Cash</span>
+                            <span className="text-lg mr-3 text-[#cfcfcf]">Jazz Cash</span>
 
                             <input type="radio" name="hs-radio-on-right" className="mt-0.5 accent-[#B600D4] border-gray-200 rounded-full text-blue-600 " />
                         </label>
@@ -38,7 +38,7 @@ const AddAccount = () => {
                             <span className='h-8 w-8 p-[3px] bg-white rounded-md mr-2'>
                                 <img className='rounded-md' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRK5OMeGQnlZ-I81VAAluwyoYy7-62iQP11lGtti4qaxg&s" alt="" />
                             </span>
-                            <span className="text-lg mr-3 text-gray-500 text-[#cfcfcf]">Easy Paisa</span>
+                            <span className="text-lg mr-3 text-[#cfcfcf]">Easy Paisa</span>
 
                             <input type="radio" name="hs-radio-on-right" className="mt-0.5 accent-[#B600D4] border-gray-200 rounded-full text-blue-600 " />
                         </label>
